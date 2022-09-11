bffb413909c424e582cb55391d2359bd607ad987
vibovenkat123 <vibovenkat@gmail.com>
2022-09-10 22:49:52 -0500

    setup typescript and the classes

---
+++ script.js
@@ -0,0 +1,10 @@
+var LinkedList = /** @class */ (function () {
+    function LinkedList() {
+    }
+    return LinkedList;
+}());
+var nodeClass = /** @class */ (function () {
+    function nodeClass(value) {
+    }
+    return nodeClass;
+}());
---
+++ script.ts
@@ -0,0 +1,8 @@
+class LinkedList {
+
+}
+class nodeClass {
+  constructor(value){
+
+  }
+}
