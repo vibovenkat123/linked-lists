31d2754885abb7988ab0b5dc58baa888c21f00df
vibovenkat123 <vibovenkat@gmail.com>
2022-09-11 11:17:08 -0500

    add append method

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-class LinkedList {
-    head = null;
-}
 class nodeClass {
     value;
     nextNode;
     constructor(value) {
         this.value = value;
         this.nextNode = null;
+    }
+}
+class LinkedList {
+    head = null;
+    constructor(headNode) {
+        this.head = headNode;
+        this.head.nextNode = null;
+    }
+    append(value) {
+        let newNode = new nodeClass(value);
+        let test = this.head;
+        this.head.nextNode = newNode;
     }
 }
--- script.ts
+++ script.ts
@@ -1,12 +1,20 @@
-class LinkedList {
-  head: any = null;
-  
-}
 class nodeClass {
   value:any;
   nextNode:any;
   constructor(value:any){
     this.value = value;
     this.nextNode = null;
+  }
+}
+class LinkedList {
+  head:any = null
+  constructor(headNode:any){
+    this.head = headNode;
+    this.head.nextNode = null
+  }
+  append(value:any){
+    let newNode = new nodeClass(value);
+    let test = this.head;
+    this.head.nextNode = newNode
   }
 }
